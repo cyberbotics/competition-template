631415d039f3166b199830d4c08663bc3bf9d6f7
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-12-15 11:56:06 +0100

    Renamed benchmark to competition

--- plugins/robot_windows/robot_programming_competition/robot_programming_competition.js
+++ plugins/robot_windows/robot_programming_competition/robot_programming_competition.js
@@ -1,38 +1,39 @@
 import RobotWindow from 'https://cyberbotics.com/wwi/R2022b/RobotWindow.js';
 
 window.robotWindow = new RobotWindow();
-const benchmarkName = 'Robot Programming';
-let benchmarkPerformance = 0;
+const competitionName = 'Robot Programming';
+let competitionPerformance = 0;
 
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".close-button");
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+  modal.classList.toggle("show-modal");
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
+  if (event.target === modal) {
+    toggleModal();
+  }
 }
 
 closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick); 
+window.addEventListener("click", windowOnClick);
 
 window.robotWindow.receive = function(message, robot) {
   if (message.startsWith('percent:'))
     document.getElementById('achievement').innerHTML = metricToString(parseFloat(message.substr(8)));
   else if (message.startsWith('success:')) {
-    let benchmarkPerformance = message.split(':')[2]
-    const benchmarkPerformanceString = metricToString(benchmarkPerformance);
-    document.getElementById('achievement').innerHTML = benchmarkPerformanceString;
+    let competitionPerformance = message.split(':')[2]
+    const competitionPerformanceString = metricToString(competitionPerformance);
+    document.getElementById('achievement').innerHTML = competitionPerformanceString;
     document.getElementById('achievement').style.color = 'green';
-    document.querySelector(".text").innerHTML = `
-      <h2>${benchmarkName} complete</h2>
-      <h3>Congratulations you finished the benchmark!</h3>
-      <p>Your current performance is: <b style="color:green;">${benchmarkPerformanceString}</b></p>
-      <p>If you want to submit your controller to the leaderboard, follow the instructions given by the "Register" button on the benchmark page.</p>
+    document.querySelector(".text").innerHTML =
+      `
+      <h2>${competitionName} complete</h2>
+      <h3>Congratulations you finished the competition!</h3>
+      <p>Your current performance is: <b style="color:green;">${competitionPerformanceString}</b></p>
+      <p>If you want to submit your controller to the leaderboard, follow the instructions given by the "Register" button on the competition page.</p>
     `
     toggleModal()
   } else

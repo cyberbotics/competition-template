c30dc562686287694d221828dad3abc45771ed20
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-01-13 10:21:29 +0100

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -16,6 +16,9 @@ print("Move the motors of the Thymio II to position " + str(target) + ".")
 robot.getDevice("motor.left").setPosition(target)
 robot.getDevice("motor.right").setPosition(target)
 
+# Run the simulation for 10 seconds
+robot.step(10000)
+
 # This is the simplest controller that works for this competition
 # If you want to experiment with more complex functions, you can read the programming guide here:
 # https://www.cyberbotics.com/doc/guide/controller-programming?tab-language=python
